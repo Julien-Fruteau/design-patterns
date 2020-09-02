96da48048c777a98fde6d4cf1571345ff43b8606
Julien-Fruteau <julien.fruteau@gmail.com>
2020-09-02 10:12:09 +0200

    behaviour-observer design

---
+++ src/observer/__main__.py
@@ -0,0 +1,12 @@
+from src.observer.observer_dashboard_current_kpis import DashboardCurrentKPIs
+from src.observer.subject_kpis import KPIs
+
+kpis = KPIs()
+curr_kpis = DashboardCurrentKPIs(kpis)  # attach itself to kpis
+kpis.set_kpis(25, 10, 5)
+
+curr_kpis.display()
+kpis.deattach(curr_kpis)
+
+# kpis._observers
+# kpis.attach(curr_kpis)
