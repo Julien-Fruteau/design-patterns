cc3d1b41b31b422fb057a21e3988cf07314a7166
Julien-Fruteau <julien.fruteau@gmail.com>
2021-01-15 12:07:39 +0100

    refact: observer, use all context manager

--- src/observer/AObserver.py
+++ src/observer/AObserver.py
@@ -7,9 +7,11 @@ class AObserver(metaclass=abc.ABCMeta):
     def update(self, value: Union[int, None] = None):
         pass
 
+    # turn the class into a context manager (with ...)
     def __enter__(self):
         return self
 
+    # turn the class into a context manager (with ...)
     @abc.abstractmethod
     def __exit__(self, exception_type, exception_value, traceback):     # type: ignore
         pass
--- src/observer/__main__.py
+++ src/observer/__main__.py
@@ -2,11 +2,9 @@ from src.observer.Observer import CurrentKPIs
 from src.observer.Subject import KPIs
 
 
-with KPIs() as kpis:                # kpis is the subject
-    curr_kpis = CurrentKPIs(kpis)   # observer curr_kpis, attach itself to kpis
-    kpis.set_kpis(25, 10, 5)        # initialise subject with values
-    curr_kpis.display()             # use obeserver that has been updated
-    kpis.deattach(curr_kpis)        # detach observer
-
-# kpis._observers
-# kpis.attach(curr_kpis)
+with KPIs() as kpis:                        # kpis is the subject, (with) benefit context manager implementation of Subject class
+    with CurrentKPIs(kpis) as curr_kpis:    # benefit Observer Context Manager implementation
+        # curr_kpis = CurrentKPIs(kpis)     # observer curr_kpis, attach itself to kpis
+        kpis.set_kpis(25, 10, 5)            # initialise subject with values, NOTE that it update Observer attached at the same time
+        curr_kpis.display()                 # use obeserver that has been updated
+        # kpis.deattach(curr_kpis)          # detach observer. NOTE that curr_kpis automatically detach itself with __exit__ method
